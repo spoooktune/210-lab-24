5f1f6c3904e20218f48828d49bb3b3f6b7f96486
spoooktune <asriprasana5@gmail.com>
2025-10-26 20:34:22 -0700

    Added main_menu function

--- main.cpp
+++ main.cpp
@@ -13,6 +13,23 @@ void add_goat(list<Goat> &trip, string [], string []);
 void display_trip(list<Goat> trip);
 int main_menu();
 
+int main_menu(){
+    int choice = 0;
+    while (choice != 1 && choice != 2 && choice != 3 && choice != 4){
+        cout << "--- Goat Manager 3001 ---" << endl;
+        cout << "[1] Add a goat" << endl;
+        cout << "[2] Delete a goat" << endl;
+        cout << "[3] List goats" << endl;
+        cout << "[4] Quit" << endl << endl;
+        cout << "Please enter an option: ";
+        cin >> choice;
+        if (choice != 1 && choice != 2 && choice != 3 && choice != 4){
+            cout << "Invalid choice. Please choose one of the specified options." << endl;
+        }
+    }
+    return choice;
+}
+
 int main() {
     srand(time(0));
     bool again;
@@ -29,8 +46,7 @@ int main() {
     while (fin1 >> colors[i++]);
     fin1.close();
 
-
-
+    int test = main_menu();
 
     return 0;
 }
